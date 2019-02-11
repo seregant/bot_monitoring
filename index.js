80d613d8f58ec0167c42f69065668177bb95190b
seregant <indraagus@live.com>
2019-02-11 09:07:44 +0700

    Fix multiuser

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const token = config.bot_token
 const bot = new TelegramBot(token, {polling: true})
 const Promise = require('promise')
 const request = require('request')
+const interval = 5000, interval_in_ms = interval * 60 * 1000
+const thershold = 90
 bot.on('message', (msg) => {
     // console.log(msg) 
     if(config.owner_username.includes(msg.from.username)){
@@ -62,25 +64,30 @@ bot.on('callback_query', function onCallbackQuery(server_choosen) {
     })
     
     collectServer.then((server)=>{
-        request({
+        monitorSrv(server,bot,server_choosen)
+    })
+})
+
+function monitorSrv(server,bot,server_choosen){
+    request({
             url:'http://'+server.host+':'+server.port+'/sysinfo',
             headers:{
                 'Authorization': 'Bearer '+ config.server_key
             }
         },(err, res, body)=>{
-            let result = JSON.parse(body)
+            var result = JSON.parse(body)
             result = result.data
             let data = ''
-            data += "Server : "+ server.name +"\n\n"
-            data += "Platform: "+ result.platform + "\n"
-            data += "CPU : " +result.cpu + "\n"
-            data += "Number of CPUs: " +result.num_cpu + "\n"
-            data += "CPU Usage (%) : " + result.cpu_usage + "\n"
-            data += "Total Memory: " + result.total_mem + "\n"
-            data += "Free Memory: " + result.free_mem + "\n"
-            data += "System Uptime: " + result.uptime + "\n"
+            data += "Server   : "+ server.name +"\n\n"
+            data += "Platform : "+ result.platform + "\n"
+            data += "CPU      : " +result.cpu + "\n"
+            data += "Number of CPUs : " +result.num_cpu + "\n"
+            data += "CPU Usage (%)  : " + result.cpu_usage + "\n"
+            data += "Total Memory   : " + result.total_mem + "\n"
+            data += "Free Memory    : " + result.free_mem + "\n"
+            data += "System Uptime  : " + result.uptime + "\n"
             data += "\n"
-            data += "Disk Size: " + result.disk.total + "\n"
+            data += "Disk Size      : " + result.disk.total + "\n"
             data += "Disk Free Space: " + result.disk.free + "\n"
             let opts = {
                 chat_id: server_choosen.message.chat.id,
@@ -88,10 +95,14 @@ bot.on('callback_query', function onCallbackQuery(server_choosen) {
                 parse_mode: "HTML"
             }
             bot.editMessageText(data, opts);
-            // bot.sendMessage(server_choosen.message.chat.id, data,{})
         })
-    })
-})
+}
+
+setInterval(function(){
+    for (i=0; i < config.server.length; i++){
+        console.log(config.server[i].name)
+    }
+}, interval)
 
 app.use(bodyParser.json())
 console.log('Telegram Bot server is running')
